b5e60369caa782a399dbd017581f0a7c81639672
Arju Mungusmare <arjumungusmare@gmail.com>
2026-09-14 09:24:34 +0530

    Added Article components

---
+++ src/assets/Components/Artical.jsx
@@ -0,0 +1,8 @@
+function Artical() {
+    return (
+        <article className="main">
+            <p>Here you can add your, website content.</p>
+        </article>
+    );
+}
+export default Artical;
--- src/assets/Components/Main.jsx
+++ src/assets/Components/Main.jsx
@@ -1,9 +1,13 @@
+import Artical from "./Artical";
+
 function Main() {
     return (
         <main className="main">
             <h3>Welcome to My Website</h3>
             <p>This is the Main content</p>
-            <p> Here you can add your<br></br>website content.</p>
+            <Artical />
+            <Artical />
+            <Artical />
         </main>
     );
 }
